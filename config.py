262fa86ea0afd4ed37fc15a777671ae15a5c4c02
Fyrsta <zhaoyuweiteddy@163.com>
2024-08-12 23:42:18 +0800

    update deepseek-coder result

--- config.py
+++
@@ -1,9 +0,0 @@
-chatgpt_base_url = "https://api.openai-proxy.org/v1"
-chatgpt_api_key = "sk-GvOk6CxgKDyCT7pg2eX792Zq8Zz1bfna3QlRYufBVsfQb2qE"
-chatgpt_model = "gpt-4o-mini"
-
-deepseek_base_url = "https://api.deepseek.com"
-deepseek_api_key = "sk-147e1d1954bb413d90f78a0b84eaf11b"
-deepseek_model = "deepseek-coder"
-
-asac_path = "/Users/zhaoyuwei/Desktop/LLM_on_ASAC/ASAC"
